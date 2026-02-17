d1883f91a3d82b2e40bea33a8c179ab6f19a0ec8
veridianByte <picaun1216@gmail.com>
2026-02-17 13:57:53 +0800

    Site updated: 2026-02-17 13:57:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771306603703";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771307867017";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
