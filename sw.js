aa084ee6ca67ae80fc695dd381e602d332c1cbff
root <root@DESKTOP-CVTK28G.localdomain>
2026-02-23 00:18:14 +0800

    Site updated: 2026-02-23 00:18:14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771776120243";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771777012318";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
