650ea9f334c15d9223018fef2e935bb648415406
Jiahui Bi <97780220+Picaun@users.noreply.github.com>
2026-03-13 19:52:54 +0800

    Site updated: 2026-03-13 19:52:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772839488028";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773402771371";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
